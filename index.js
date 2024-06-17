2bdff6f105ea887d5cf5f296f2f136a19bce9bf8
kaaylevirmar <kylevirmarmillendez@gmail.com>
2024-06-17 19:44:39 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+
+app.get('/',(req,res)=>{
+        res.send('hello world');
+})
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running in ${PORT}`);
+})
