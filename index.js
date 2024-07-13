6f25210332e20aed02b68e30ae203024a51c5dd6
kylevirmarmillendez <kylevirmar.millendez@nabepero.co.jp>
2024-07-13 22:18:53 +0800

    update: connected express app to mongodb

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = 3001;
 
 
+
 app.get('/',(req,res)=>{
-        res.send('hello world');
+        res.send('hello asdsadsad');
 })
 
 
 
 app.listen(PORT,()=>{
     console.log(`Server is running in ${PORT}`);
+    mongoose.connect('mongodb://mongo:27017/my-app')
+    .then((res)=>{
+        console.log('Connected to mongodb database');
+    })
+    .catch((e)=>{
+        console.log(e);
+    })
+
+    
 })
