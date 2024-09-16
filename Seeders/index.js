d4c49874affc8a8fc7ffe69104fec72b955d7bbf
kaaylevirmar <kylevirmarmillendez@gmail.com>
2024-09-16 22:50:56 +0800

    update: seeder with hashed password

--- Seeders/index.js
+++ Seeders/index.js
@@ -1,5 +1,6 @@
 const User = require('../Models/User');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const {users} = require('./data');
 
 mongoose.connect('mongodb://mongo:27017/css-db')
@@ -16,8 +17,14 @@ const DBseed = async () => {
         await User.deleteMany({});
         console.log('Old users removed');
 
-        await User.insertMany(users);
-        console.log('New users added to the database');
+        const hashedUsers = await Promise.all(users.map(async (user) => {
+            const saltRounds = 10;
+            const hashedPassword = await bcrypt.hash(user.password, saltRounds);
+            return { ...user, password: hashedPassword }; 
+        }));
+
+        await User.insertMany(hashedUsers);
+        console.log('New users with hashed passwords added to the database');
         
     } catch (error) {
         console.error('Error during seeding:', error);
